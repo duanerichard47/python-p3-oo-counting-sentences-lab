81ac976808940205693b3a48d8886eee1e44314f
Duane <duanerichard47@hotmail.com>
2023-02-25 17:45:45 -0500

    complete

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -5,15 +5,10 @@ class MyString:
     def __init__(self, value = ""):
         self._value = value
         
-    
-
     def get_value(self):
         return self._value
 
     def set_value(self, value):
-        # self._value = value
-        # print(self.input_value)
-       
         if type(value) == str:
             self._value = value
         else: 
@@ -22,17 +17,15 @@ class MyString:
     value = property(get_value, set_value)
 
     def is_sentence(self):
+        return self.value.endswith(".")
         # self.value = value
         # self.value_to_list = [letter for letter in self.value]
         # print(self.value_to_list)
         # period = '.'
         # if period in self.value_to_list:
       
-        return self.value.endswith(".")
-        
-      
-
     def is_question(self):
+        return self.value.endswith("?")
         # self.value = value
         # self.value_to_list = [letter for letter in self.value]
         # question_mark ='?'
@@ -40,9 +33,10 @@ class MyString:
         #     return True
         # else:
         #     return False
-         return self.value.endswith("?")
+         
     
     def is_exclamation(self):
+        return self.value.endswith("!")
         # self.value = value
         # self.value_to_list = [letter for letter in self.value]
         # exclamation = "!"
@@ -51,17 +45,18 @@ class MyString:
         #     return True
         # else:
         #     return False
-         return self.value.endswith("!")
+         
 
     def count_sentences(self):
         if len(self.value) == 0:
             return 0
-        splitted_value = self.value.replace('?', '.')
-        splitted_value = splitted_value.replace('!', '.')
-      
-        splitted_value = splitted_value.split('. ')
-        print(splitted_value)
-        print(len(splitted_value))
-        length = len(splitted_value)
-        return length
+        else:
+            splitted_value = self.value.replace('?', '.')
+            splitted_value = splitted_value.replace('!', '.')
+            splitted_value = splitted_value.split('. ')
+            print(splitted_value)
+            print(len(splitted_value))
+            length = len(splitted_value)
+            return length
+
         
